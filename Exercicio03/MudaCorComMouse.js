0514a37d7498bbc71f276eba075688717feae69e
Volnei Fuzer <volnei.fuzer@gmail.com>
2016-11-16 18:05:13 -0300

    finalizacao do exercicio 3

--- Exercicio03/MudaCorComMouse.js
+++
@@ -1,8 +0,0 @@
-
-function mouseOver(){
-    document.getElementById("divTeste").style.color = "#ff0000"
-} 
-
-function mouseOut() {
-    document.getElementById("divTeste").style.color = "#000000"
-}
---
+++ Exercicio03/toUpOrLowerCase.js
@@ -0,0 +1,8 @@
+
+function uperCase(){
+     document.getElementById("divTeste").innerHTML  = document.getElementById("divTeste").innerHTML.toUpperCase();
+} 
+
+function lowerCase() {
+    document.getElementById("divTeste").innerHTML  = document.getElementById("divTeste").innerHTML.toLowerCase();
+}
